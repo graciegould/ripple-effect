3b7ac04d445e73fcad0decd968db44b92e8c0bcf
“graciegould” <graciegould5@gmail.com>
2026-01-17 17:26:30 -0800

    set up project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className={styles.app}>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/RippleEffect/index.ts
@@ -0,0 +1,3 @@
+export { RippleEffect, default } from './RippleEffect';
+export type { RippleEffectProps } from './types';
+
